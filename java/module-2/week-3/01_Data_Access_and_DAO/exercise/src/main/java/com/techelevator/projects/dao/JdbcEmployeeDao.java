3daaf8f26a3ee09deef6595e0d5f28434ae01980
Calesta Holloway <calestak99@icloud.com>
2022-11-27 15:11:13 -0500

    attempt 3

--- java/module-2/week-3/01_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcEmployeeDao.java
+++ java/module-2/week-3/01_Data_Access_and_DAO/exercise/src/main/java/com/techelevator/projects/dao/JdbcEmployeeDao.java
@@ -22,18 +22,18 @@ public class JdbcEmployeeDao implements EmployeeDao {
 	public List<Employee> getAllEmployees() {
 		List<Employee> employees = new ArrayList<>();
 		String sql = "SELECT employee_id, department_id, first_name, last_name, birth_date, hire_date " +
-				"FROM employee";
+				"FROM employee JOIN project_employee ON employee.employee_id = project_employee.employee_id";
 
 		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
 		while (results.next()) {
 			employees.add(mapRowToEmployee(results));
-
 		}
 		return employees;
 	}
 
 	private Employee mapRowToEmployee(SqlRowSet results) {
 		Employee employee = new Employee();
+		employee.setId(results.getInt("employee_id"));
 		employee.setDepartmentId(results.getInt("department_id"));
 		employee.setFirstName(results.getString("first_name"));
 		employee.setLastName(results.getString("last_name"));
@@ -45,12 +45,12 @@ public class JdbcEmployeeDao implements EmployeeDao {
 	@Override
 	public List<Employee> searchEmployeesByName(String firstNameSearch, String lastNameSearch) {
 		List<Employee> employeeName = new ArrayList<>();
-		String sql = "SELECT employee_id, department_id, first_name, last_name, birth_date, hire_date " +
-				"FROM employee WHERE employee_id = ? ORDER BY firs_name, last_name";
-		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, firstNameSearch, lastNameSearch);
-		while (results.next()) {
-			Employee employee = mapRowToEmployee(results);
-			employeeName.add(employee);
+		String sqlSearchEmployeesByName = "SELECT employee_id, department_id, first_name, last_name, birth_date, hire_date " +
+				"FROM employee " + "WHERE first_name = ? AND last_name = ? ";
+		SqlRowSet results = jdbcTemplate.queryForRowSet(sqlSearchEmployeesByName, firstNameSearch, lastNameSearch);
+		while(results.next()) {
+			Employee employeeByName = mapRowToEmployee(results);
+			employeeName.add(employeeByName);
 		}
 		return employeeName;
 	}
@@ -59,11 +59,11 @@ public class JdbcEmployeeDao implements EmployeeDao {
 	public List<Employee> getEmployeesByProjectId(int projectId) {
 		List<Employee> employeeProject = new ArrayList<>();
 		String sql = "SELECT employee_id, department_id, first_name, last_name, birth_date, hire_date " +
-				"FROM employee WHERE project_id = ? ";
+				"FROM employee JOIN project_employee ON employee.employee_id = project_employee.employee_id WHERE project_id = ? ";
 		SqlRowSet results = jdbcTemplate.queryForRowSet(sql, projectId);
-		while (results.next()) {
-			Employee employee = mapRowToEmployee(results);
-			employeeProject.add(employee);
+		while(results.next()) {
+			Employee employeeProjectId = mapRowToEmployee(results);
+			employeeProject.add(employeeProjectId);
 		}
 		return employeeProject;
 	}
@@ -73,21 +73,21 @@ public class JdbcEmployeeDao implements EmployeeDao {
 		String sql = "INSERT INTO project (employee_id, department_id, first_name, last_name, birth_date, hire_date) " +
 				"VALUES (?, ?, ?, ?, ?, ?) " +
 				"RETURNING project_id;";
-
+		jdbcTemplate.update(sql, projectId, employeeId);
 
 	}
 
 	@Override
 	public void removeEmployeeFromProject(int projectId, int employeeId) {
-		String deleteEmployeeSql = "DELETE FROM project WHERE projectId = ?;";
+		String deleteEmployeeSql = "DELETE FROM project (employee_id, department_id, first_name, last_name, birth_date, hire_date JOIN employee ON project_employee.employee_id = employee.employee_id WHERE project_employee = (SELECT employee_id FROM employee WHERE project_id = ?;";
 		jdbcTemplate.update(deleteEmployeeSql, projectId);
 	}
 
 	@Override
 	public List<Employee> getEmployeesWithoutProjects() {
 		List<Employee> employeeNoProject = new ArrayList<>();
-		String sql = "SELECT employeeId, departmentId, firstName, lastName, birthDate, hireDate " +
-				"FROM employee WHERE projectId != ? ";
+		String sql = "SELECT employee_id, department_id, first_name, last_name, birth_date, hire_date , project_employee.project_id FROM employee " +
+				"LEFT JOIN project_employee ON project_employee.employee_id = employee.employee_id WHERE project_employee.project_id IS NULL ";
 		SqlRowSet results = jdbcTemplate.queryForRowSet(sql);
 		while (results.next()) {
 			Employee employee = mapRowToEmployee(results);
