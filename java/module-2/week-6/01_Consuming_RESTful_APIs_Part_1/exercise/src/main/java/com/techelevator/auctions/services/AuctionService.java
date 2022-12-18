4e78bcbdf404a4a2f7c80c9e87882497baf2ab59
Calesta Holloway <calestak99@icloud.com>
2022-12-18 14:39:48 -0500

    homework 1

--- java/module-2/week-6/01_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ java/module-2/week-6/01_Consuming_RESTful_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -7,27 +7,30 @@ import com.techelevator.auctions.model.Auction;
 public class AuctionService {
 
     public static String API_BASE_URL = "http://localhost:3000/auctions/";
+    public static String API_TITLE_URL = "http://localhost:3000/auctions?title_like=";
+    public static String API_PRICE_URL = "http://localhost:3000/auctions?currentBid_lte=";
+
     private RestTemplate restTemplate = new RestTemplate();
 
 
     public Auction[] getAllAuctions() {
         // call api here
-        return null;
+        return restTemplate.getForObject(API_BASE_URL, Auction[].class);
     }
 
     public Auction getAuction(int id) {
         // call api here
-        return null;
+        return restTemplate.getForObject(API_BASE_URL + id, Auction.class);
     }
 
     public Auction[] getAuctionsMatchingTitle(String title) {
         // call api here
-        return null;
+        return restTemplate.getForObject(API_TITLE_URL + title, Auction[].class);
     }
 
     public Auction[] getAuctionsAtOrBelowPrice(double price) {
         // call api here
-        return null;
+        return restTemplate.getForObject(API_PRICE_URL + price, Auction[].class);
     }
 
 }
