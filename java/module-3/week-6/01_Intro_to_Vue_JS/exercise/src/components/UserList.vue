58fbd4f973f1068bf2a826bacb1ed6f390b32edc
Calesta Holloway <calestak99@icloud.com>
2023-04-21 19:44:06 -0400

    homework redo

--- java/module-3/week-6/01_Intro_to_Vue_JS/exercise/src/components/UserList.vue
+++ java/module-3/week-6/01_Intro_to_Vue_JS/exercise/src/components/UserList.vue
@@ -23,12 +23,7 @@
           </select>
         </td>
       </tr>
-      <tr v-for="(user, index) in users" :key="index"/>
-      <tr
-        v-for="user in filteredList"
-        v-bind:key="user.userName"
-        v-bind:class="{ disabled: user.status === 'Inactive' }"
-      >
+      <tr v-for="(user, index) in users" :key="index">
         <td>{{ user.firstName }}</td>
         <td>{{ user.lastName }}</td>
         <td>{{ user.username }}</td>
